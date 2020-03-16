2e35e967ea4d3fb0bf30d163e781f4abbb7b1541
orkamara <orkamara@gmail.com>
2020-03-16 18:40:08 +0200

    feat: add templated functionality for issuePathToLineNumber

--- test/lib/index.test.ts
+++ test/lib/index.test.ts
@@ -1,5 +1,37 @@
-import fun from "../../lib";
+import { issuePathToLineNumber } from "../../lib/issue-to-line";
+import { CloudConfigFileTypes } from "../../lib/types";
 
-test("Is everything ready for the development?", async () => {
-  expect(fun()).toBeTruthy();
+const dumyFileContent = "dumy";
+const dumyPath = "dumy";
+
+describe("issuePathToLineNumber", () => {
+  test("YAML file", () => {
+    expect(
+      issuePathToLineNumber(
+        dumyFileContent,
+        CloudConfigFileTypes.YAML,
+        dumyPath
+      )
+    ).toEqual(0);
+  });
+
+  test("JSON - not supported", () => {
+    expect(() => {
+      issuePathToLineNumber(
+        dumyFileContent,
+        CloudConfigFileTypes.JSON,
+        dumyPath
+      );
+    }).toThrowError("JSON format is not supported");
+  });
+
+  test("Unsupported type", () => {
+    expect(() => {
+      issuePathToLineNumber(
+        dumyFileContent,
+        CloudConfigFileTypes.JSON + 100,
+        dumyPath
+      );
+    }).toThrowError("Unknown format");
+  });
 });
